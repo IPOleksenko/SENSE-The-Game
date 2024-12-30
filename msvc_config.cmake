262b429e7970ef7dbdb524825c46382764294848
Vortm4x <maks.trehubenko@gmail.com>
2024-12-30 03:50:46 +0200

    fix dll not found for msvc

--- msvc_config.cmake
+++ msvc_config.cmake
@@ -12,6 +12,15 @@ target_link_options(${PROJECT_NAME}
         $<$<CONFIG:Debug>:/SUBSYSTEM:CONSOLE>
 )
 
+# Copy DLL files to the build directory (if applicable)
+add_custom_command(
+    TARGET  ${PROJECT_NAME} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:SDL2::SDL2> $<TARGET_FILE_DIR:${PROJECT_NAME}>
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:SDL2_image::SDL2_image> $<TARGET_FILE_DIR:${PROJECT_NAME}>
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:SDL2_ttf::SDL2_ttf> $<TARGET_FILE_DIR:${PROJECT_NAME}>
+    COMMENT "Copying DLL files to the binary directory"
+)
+
 # Set the default startup project in Visual Studio
 set_directory_properties(
     ${CMAKE_CURRENT_SOURCE_DIR} PROPERTIES
